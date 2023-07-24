a56b53ae41b0e726d3e5283718c5858a4e6ae9e2
pastaChaeng <arlenejoy.nacion777@gmail.com>
2023-07-24 13:51:36 +0800

    side-nav(1)

--- twt-app/src/app/page.tsx
+++ twt-app/src/app/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import {BiHomeCircle, BiUser} from 'react-icons/bi';
-import {BsBell, BsBookmark} from 'react-icons/bs';
+import {BsBell, BsBookmark, BsThreeDots, BsTwitter} from 'react-icons/bs';
 import {HiOutlineHashtag} from 'react-icons/hi';
 import {HiEnvelope} from 'react-icons/hi2';
 
 const NAVIGATION_ITEMS = [
+  {
+    title:'Twitter',
+    icon:BsTwitter
+  },
   {
     title:'Home',
     icon:BiHomeCircle
@@ -35,23 +39,38 @@ const NAVIGATION_ITEMS = [
 const Home = () => {
   return (
     <div className='w-full h-full flex justify-center items-center relative bg-black'>
-      <div className='max-w-screen-lg w-full h-full flex relative'>
+      <div className='max-w-screen-xl w-full h-full flex relative'>
       {/* left sidebar nav/header*/}
-      <section className='fixed w-72 flex flex-col'>
+      <section className='fixed w-[275px] flex flex-col items-stretch h-screen'>
+        <div className="flex flex-col items-stretch h-full space-y-4 mt-4">
       {
         NAVIGATION_ITEMS.map((item)=>(
-          <Link className='bg-white/50 flex item-center justify-center space-x-2 rounded-3xl p-4' href={'/$(item.title.toLowerCase())'} key={item.title}>
+          <Link 
+          className='hover:bg-white/10 text-2xl transition duration-200 flex item-center justify-start w-fit space-x-4 rounded-3xl p-2 px-6'
+          href={'/${item.title.toLowerCase()}'} 
+          key={item.title}>
             <div>
               <item.icon/>
             </div>
-            <div>
               {
-                item.title
+                item.title !== "Twitter" && <div>{item.title}</div>
               }
-            </div>
           </Link>
         ))
       }
+      <button className="rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200">Tweet</button>
+      </div>
+      <button className="rounded-full flex items-center space-x-2 m-4 bg-transparent p-4 text-center hover:bg-white/10
+      transition duration-200">
+      <div className="rounded-full bg-slate-400 w-8 h-8"></div>
+      <div className="text-left text-xs">
+      <div className="font-semibold">Lazy Coder</div>
+      <div className="">@lazycoderdot</div>
+      </div>
+      <div>
+      <BsThreeDots/>
+      </div>
+      </button>
       </section>
       <main></main>
       <section></section>
